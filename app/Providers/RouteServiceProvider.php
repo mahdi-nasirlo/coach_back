fd6f778da8e7b46bffbaa4e7084b38b63239eb88
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-12-08 22:09:32 +0330

    FIX:: handle cross

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,7 +28,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::prefix('/api/v1')
-                ->middleware('api')
+                ->middleware(['api', 'cors'])
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api/v1/api.php'));
 
