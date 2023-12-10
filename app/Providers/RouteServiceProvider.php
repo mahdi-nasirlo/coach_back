064f40f4b5db5b5cdcebcd00b8228e69c0cb6a18
mahdi <m.nwordpress1223@gmail.com>
2023-12-10 20:36:03 +0330

    fix:: error 500

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,7 +28,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::prefix('/api/v1')
-                ->middleware(['api', 'cors'])
+                ->middleware(['api'])
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api/v1/api.php'));
 
